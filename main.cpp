370cb72a26b84d21b177632a26294da586fd14d3
Kanta Tamura <kanta03085@gmail.com>
2021-05-23 15:11:33 +0900

    add: read write functions

---
+++ main.cpp
@@ -0,0 +1,83 @@
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Image {
+    cv::Mat img;
+    vector< vector< unsigned char > > pix_val;
+    int img_height{}, img_width{};
+
+public:
+    Image() = default;
+    void readImage(const string& file_name);
+    void copyImage(const Image& base_image);
+    void writeImage(const string& file_name);
+};
+
+int main(int argc, char* argv[]) {
+    Image input_img, output_img;
+
+    input_img.readImage("../SIDBA_Gray/LENNA.jpg");
+    output_img.copyImage(input_img);
+    output_img.writeImage("../LENNA_result.jpg");
+
+    return 0;
+}
+
+void Image::readImage(const string& file_name) {
+    // read grayscale image
+    img = cv::imread(file_name, cv::IMREAD_GRAYSCALE);
+    // error handling
+    if (img.empty()) {
+        cerr << "Failed to open image file : " << file_name << endl;
+        exit(EXIT_FAILURE);
+    } else {
+        cout << "Read success : " << file_name << endl;
+    }
+
+    // save image pixel size
+    img_height = img.size().height; img_width = img.size().width;
+
+    // save image pixel values
+    pix_val.resize(img_height, vector<unsigned char>(img_width));
+    for (int height = 0; height < img_height; ++height) {
+        for (int width = 0; width < img_width; ++width) {
+            pix_val[height][width] = img.data[img_width * height + width];
+        }
+    }
+}
+
+void Image::copyImage(const Image& base_image) {
+    // copy image data
+    img = base_image.img.clone();
+    // error handling
+    if (img.empty()) {
+        cerr << "Failed to copy image file" << endl;
+        exit(EXIT_FAILURE);
+    } else {
+        cout << "Copy success" << endl;
+    }
+
+    img_height = base_image.img_height; img_width = base_image.img_width;
+
+    // copy image pixel values
+    pix_val = base_image.pix_val;
+}
+
+void Image::writeImage(const string& file_name) {
+    // load pixel values
+    for (int height = 0; height < img_height; ++height) {
+        for (int width = 0; width < img_width; ++width) {
+            img.data[img_width * height + width] = pix_val[height][width];
+        }
+    }
+
+    // write image
+    cv::imwrite(file_name, img);
+
+    std::cout << "Write : " << file_name << std::endl;
+}
