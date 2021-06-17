fbb288b04aded3adf747c72f6c5f8c38f58be6bb
KantaTamura <tkanta496@gmail.com>
2021-06-17 22:44:04 +0900

    add: discriminant method

--- main.cpp
+++ main.cpp
@@ -2,12 +2,12 @@
 Image image;
 
 int main(int argc, char* argv[]) {
-    std::string input_img_name = "Tree", output_img_name = "p_tail_30";
+    std::string input_img_name = "Tree", output_img_name = "discriminant";
     image
         .readImage("../SIDBA_Gray/" + input_img_name + ".jpg")
         .writeImage("../result/" + input_img_name + "_original.jpg")
         .makeHistogram("../result/histogram/" + input_img_name + "_histogram_original.jpg")
-        .p_tail()
+        .discriminant()
         .writeImage("../result/" + input_img_name + "_" + output_img_name + ".jpg")
         .makeHistogram("../result/histogram/" + input_img_name + "_histogram_" + output_img_name + ".jpg");
 }
--- src/binarization.cpp
+++ src/binarization.cpp
@@ -43,7 +43,36 @@ Image Image::p_tail() {
 }
 
 Image Image::discriminant() {
+    double sb2_max = 0.;
+    int threshold_result = 0;
+    for (int threshold = 0; threshold <= 255; ++threshold) {
+        int num0 = 0, sum0 = 0;
+        int num1 = 0, sum1 = 0;
+        for (int height = 0; height < img_height; ++height) {
+            for (int width = 0; width < img_width; ++width) {
+                if (pix_val[height][width] < threshold) {
+                    sum0 += pix_val[height][width];
+                    num0++;
+                } else {
+                    sum1 += pix_val[height][width];
+                    num1++;
+                }
+            }
+        }
 
+        double m0 = 0., m1 = 0.;
+        if (num0 == 0)  m0 = 0.;
+        else    m0 = sum0 / num0;
+        if (num1 == 0)  m1 = 0.;
+        else    m1 = sum1 / num1;
+
+        double sb2 = num0 * num1 * (m0 - m1) * (m0 - m1);
+        if (sb2 > sb2_max) {
+            sb2_max = sb2;
+            threshold_result = threshold;
+        }
+    }
+    return this->binarization(threshold_result);
 }
 
 Image Image::expantion() {
